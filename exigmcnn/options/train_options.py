e95dc184e7a893312738e8ee502766edb261be09
zhengyi1212 <zhuzhengyi2013@126.com>
2020-03-06 01:50:50 +0800

    update

--- exigmcnn/options/train_options.py
+++ exigmcnn/options/train_options.py
@@ -19,14 +19,14 @@ class TrainOptions:
 
     def initialize(self):
         self.parser.add_argument('--dataset', type=str, default='places2', help='The dataset of the experiment.')
-        self.parser.add_argument('--data_imgfile', type=str, default='/home/zzy/work/exi_inpaint/eximage/files/b500imgfile.txt', help='the file storing training file paths')
-        self.parser.add_argument('--data_eximgfile', type=str, default='/home/zzy/work/exi_inpaint/eximage/files/b500eximgfile.txt', help='the file storing training file paths')
-        self.parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids: e.g. 0')
+        self.parser.add_argument('--data_imgfile', type=str, default='./eximage/files/b500imgfile.txt', help='the file storing training file paths')
+        self.parser.add_argument('--data_eximgfile', type=str, default='./eximage/files/b500eximgfile.txt', help='the file storing training file paths')
+        self.parser.add_argument('--gpu_ids', type=str, default='1', help='gpu ids: e.g. 0')
         self.parser.add_argument('--checkpoints_dir', type=str, default='./checkpoints', help='models are saved here')
         self.parser.add_argument('--load_model_dir', type=str, default='', help='pretrained models are given here')
         self.parser.add_argument('--model_prefix', type=str, default='snap', help='models are saved here')
 
-        self.parser.add_argument('--batch_size', type=int, default=8, help='input batch size')
+        self.parser.add_argument('--batch_size', type=int, default=16, help='input batch size')
 
         self.parser.add_argument('--random_mask', type=int, default=1)
         self.parser.add_argument('--mask_type', type=str, default='rect')
